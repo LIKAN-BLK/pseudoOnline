6d800a56e7fff057b29d9342569b1f23d1006368
Bogdan <likan26.09@yandex.ru>
2016-06-15 21:10:42 +0300

    added output in loop in run script

--- run.m
+++ run.m
@@ -9,6 +9,7 @@ data_path='../exp4/';
 w_size_time = 400;%400ms
 for rp_end = -2000+w_size_time:100:-500
     for rp_start = -2000:100:rp_end - w_size_time 
+        disp(sprintf('rp_start = %d,rp_end=%d\n\r',rp_start,rp_end))
         pseudoOnlineExp3(data_path,w_size_time,rp_start,rp_end)
     end
 end
